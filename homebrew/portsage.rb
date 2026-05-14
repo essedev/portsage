f9a873618c548f7e8ad160742c062380da3cbb15
Simone Salerno <doppiaesse@proton.me>
2026-05-14 19:28:16 +0200

    homebrew template: bump to v0.12.2

--- homebrew/portsage.rb
+++ homebrew/portsage.rb
@@ -1,6 +1,6 @@
 cask "portsage" do
-  version "0.12.1"
-  sha256 "775a26a0e0e2e1fd25725ff4fc7130f9a9d6c957ccf8647b3ca486478b686d04"
+  version "0.12.2"
+  sha256 "804a2fb378b40b98acecf51b7367a14f178334393d85b3ad3060cccdcaf29ac9"
 
   url "https://github.com/essedev/portsage/releases/download/v#{version}/Portsage_#{version}_aarch64.dmg"
   name "Portsage"
